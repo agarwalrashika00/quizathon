bc4555a078892e4cc16e489dff6a51e09a69b43d
Rashika <agarwalrashika00@gmail.com>
2023-05-08 19:58:00 +0530

    users and admin signup and registration using email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  enum role: { participant: 0, admin: 1 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '/', controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+    confirmations: "users/confirmations"
+  }
+
+  devise_scope :user do
+    get '/admin/sign_in' => 'admin/sessions#new'
+    post '/admin/sign_in' => 'admin/sessions#create'
+    get '/admin/sign_up' => 'admin/registrations#new'
+    post '/admin' => 'admin/registrations#create'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+
+  resources :admin
 end
